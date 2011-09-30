c04631c5927ba485ee0820d3353a82a5e9f9a2a8
Iain Dalton <iain.dalton@gmail.com>
2011-09-30 12:00:45 -0600

    Make bare headings H3

--- projects.php
+++ projects.php
@@ -2,12 +2,12 @@
 
     <h2>Club Projects</h2>
 <br />
-Current Projects
+<h3>Current Projects</h3>
 <ul>
     <li><a href="http://www.maximumpc.com/article/features/maximum_pc_builds_a_multitouch_surface_computer">Multitouch-surface computer</a> (a la Microsoft Surface)</li>
     <li>Developing and redesigning the club website.<ul><li>Layout</li><li>Art</li><li>XHTML compliance</li></ul></li>
 </ul>
-On the drawing board
+<h3>On the drawing board</h3>
 <ul>
     <li><a href="http://developer.android.com/index.html"> Android Moble App Development. </a></li>
     <li><a href="http://research.microsoft.com/en-us/um/redmond/projects/kinectsdk/"> Kinect Programming </a></li>
