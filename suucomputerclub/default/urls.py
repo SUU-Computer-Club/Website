7cc4d10fcadabcf4715d85fc1efbf9a06389ff65
geekingreen <geekingreen@locke.suu.edu>
2011-11-22 16:54:15 -0700

    fix deleted files

--- suucomputerclub/default/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import url, patterns, include
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('default.views',
-    url(r'^$', 'index', name='index'),
-)
--- suucomputerclub/default/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-# Create your views here.
-
-def index(request):
-    return render_to_response('base.html', locals(), context_instance=RequestContext(request))
